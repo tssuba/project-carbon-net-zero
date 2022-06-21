5da1d0d0c266f3dd619296f506f471c85afdd85c
Tanmay Suba <hellotanmay@protonmail.com>
2022-06-21 06:14:33 +0530

    more functionality and custom theme

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,43 +3,12 @@ import IconButton from '@mui/material/IconButton';
 import Box from '@mui/material/Box';
 import { useTheme, ThemeProvider, createTheme } from '@mui/material/styles';
 import { amber, deepOrange, grey } from '@mui/material/colors';
-import { PaletteMode } from '@mui/material';
 import Brightness4Icon from '@mui/icons-material/Brightness4';
 import Brightness7Icon from '@mui/icons-material/Brightness7';
+import getDesignTokens from './theme';
 
 
 const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
-
-
-const getDesignTokens = (mode: PaletteMode) => ({
-  palette: {
-    mode,
-    primary: {
-      ...amber,
-      ...(mode === 'dark' && {
-        main: amber[300],
-      }),
-    },
-    ...(mode === 'dark' && {
-      background: {
-        default: deepOrange[900],
-        paper: deepOrange[900],
-      },
-    }),
-    text: {
-      ...(mode === 'light'
-        ? {
-            primary: grey[900],
-            secondary: grey[800],
-          }
-        : {
-            primary: '#fff',
-            secondary: grey[500],
-          }),
-    },
-  },
-});
-
 
 
 function App() {
@@ -70,9 +39,9 @@ function App() {
         <Box
           sx={{
           display: 'flex',
-          width: '100%',
+          // width: '100%',
           alignItems: 'center',
-          justifyContent: 'center',
+          justifyContent: 'flex-end',
           bgcolor: 'background.default',
           color: 'text.primary',
           borderRadius: 1,
