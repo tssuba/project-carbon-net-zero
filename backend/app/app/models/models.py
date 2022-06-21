4c2d4d7b88431d631bf86316b8ad67f93f490806
nova-sangeeth <novasangeeth@outlook.com>
2022-06-21 14:08:53 +0530

    remove under scores

--- backend/app/app/models/models.py
+++ backend/app/app/models/models.py
@@ -1,41 +1,41 @@
-import sqlalchemy as _sql
-import sqlalchemy.orm as _orm
-import db.database as _database
-
 from sqlalchemy.sql.expression import null
 from db.database import Base
-from sqlalchemy import String, Boolean, Integer, Column, Text
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, String, Float
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
 
 
-class Article(_database.Base):
+class Article(Base):
     __tablename__ = "articles"
-    id = _sql.Column(_sql.Integer, primary_key=True, index=True)
-    link = _sql.Column(_sql.String, index=True, default="")
-    published_date = _sql.Column(_sql.String, index=True, default="")
-    publisher = _sql.Column(_sql.String, index=True, default="")
-    title = _sql.Column(_sql.String, index=True, default="")
+    id = Column(Integer, primary_key=True, index=True)
+    link = Column(String, index=True, default="")
+    published_date = Column(String, index=True, default="")
+    publisher = Column(String, index=True, default="")
+    title = Column(String, index=True, default="")
 
 
-class ResearchArticle(_database.Base):
+class ResearchArticle(Base):
     __tablename__ = "research"
-    id = _sql.Column(_sql.Integer, primary_key=True, index=True)
-    doi = _sql.Column(_sql.String, index=True, default="")
-    published_date = _sql.Column(_sql.String, index=True, default="")
-    publisher = _sql.Column(_sql.String, index=True, default="")
-    title = _sql.Column(_sql.String, index=True, default="")
+    id = Column(Integer, primary_key=True, index=True)
+    doi = Column(String, index=True, default="")
+    published_date = Column(String, index=True, default="")
+    publisher = Column(String, index=True, default="")
+    title = Column(String, index=True, default="")
 
-class TweetId(_database.Base):
+
+class TweetId(Base):
     __tablename__ = "tweetIds"
-    id = _sql.Column(_sql.Integer, primary_key=True, index=True)
-    tweetId = _sql.Column(_sql.String, index=True, default="")
+    id = Column(Integer, primary_key=True, index=True)
+    tweetId = Column(String, index=True, default="")
 
 
 class Item(Base):
-    __tablename__ = 'items'
-    id = Column(Integer, primary_key = True)
-    name = Column(String(255), nullable = False, unique = True)
+    __tablename__ = "items"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255), nullable=False, unique=True)
 
 
-#return string representation in console
+# return string representation in console
 def __repr__(self):
     return f"<Item name = {self.name}>"
