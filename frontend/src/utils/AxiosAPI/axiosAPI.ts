cce6c93b3c1c8010f7834235b02e2c45df7d477d
Tanmay Suba <hellotanmay@protonmail.com>
2022-06-28 13:35:39 +0530

    changed axios api to work with docker img

--- frontend/src/utils/AxiosAPI/axiosAPI.ts
+++ frontend/src/utils/AxiosAPI/axiosAPI.ts
@@ -1,13 +1,13 @@
 import axios from "axios";
 
-const FALLBACK_BACKEND_URL = "https://carbon-net-zero-backend.herokuapp.com";
+const FALLBACK_BACKEND_URL = "http://api.localhost";
 const BASEURL = process.env.REACT_APP_BACKEND_URL || FALLBACK_BACKEND_URL;
 
 const REQUEST_TIMEOUT = 1500;
 
 // Singleton Axios Instance
 const axiosInstance = axios.create({
-    baseURL: "https://carbon-net-zero-backend.herokuapp.com",
+    baseURL: BASEURL,
     timeout: REQUEST_TIMEOUT,
     headers: {
         "Content-Type": "application/json",
